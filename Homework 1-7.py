0d0a156725fc60879eebbe6ae106005add9b660d
maksym444 <makskryvenko@gmail.com>
2016-11-09 22:34:27 +0200

    Homework 1-7

---
+++ Homework 1-7.py
@@ -0,0 +1,52 @@
+1.
+a=10
+b=10
+c=20
+r = (a + b * ( c / 2 ))
+print (r)
+
+2.
+a=31232
+b=21533
+r =(a**2+b**2)%2
+print (r)
+
+3.
+a=10
+b=10
+c=10
+r=( a + b ) // 12 * c %4 + b
+print (r)
+
+4.
+a=10
+b=10
+c=10
+r=(a -b*c)//(a+b)%c
+print (r)
+
+5.
+import math
+a=10
+b=20
+c=30
+r=((a - b)//(a + b)**3-math.cos(c))
+print (r)
+
+6.
+import math
+a=10
+b=20
+c=30
+r=(math.log(1+c)//-b)**4+a
+print (r)
+
+
+7.
+S =data="20.10.2016"
+S1=data [:3]
+S2=data [3:6]
+S3=data [6:]
+a=S2+S1+S3,
+b=S1+S2+S3
+print (a,b)
